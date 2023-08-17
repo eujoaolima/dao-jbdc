b64100e5b32b4ba8cf4968e84fc9ec29e3001b1e
eujoaolima <joaopedro.donatolima@hotmail.com>
2023-08-17 14:09:14 -0300

    Criação da entidade Seller e criação do seu relacionamento com a entidade Department

--- src/Application/Principal.java
+++ src/Application/Principal.java
@@ -1,10 +1,16 @@
 package Application;
 
-import Model.Entities.Department;
+import Entities.Department;
+import Entities.Seller;
+
+import java.util.Date;
 
 public class Principal {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
         System.out.println(department);
+
+    Seller seller = new Seller(1, "Bob, o Bobo", "bob@gmail.com", new Date(), 3000.0, department);
+    System.out.println(seller);
     }
 }
